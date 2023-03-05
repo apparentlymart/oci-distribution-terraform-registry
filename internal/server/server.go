800443f6daad3ec5e503facedcf39a7de8d4bd30
Martin Atkins <mart@degeneration.co.uk>
2023-03-04 19:10:50 -0800

    server: Beginnings of actually handling the mirror service requests
    So far we just have an HTML page advertising the mirror. The mirror
    protocol itself will follow in later commits.

--- internal/server/server.go
+++ internal/server/server.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/apparentlymart/oci-distribution-terraform-registry/internal/config"
@@ -17,13 +19,7 @@ func Run(ctx context.Context, config *config.Config) error {
 	mux := http.NewServeMux()
 
 	for _, mirrorSvc := range config.ProviderMirrors {
-		serviceName := mirrorSvc.Name
-		prefix := "/" + mirrorSvc.Name + "/"
-		mux.HandleFunc(prefix, func(resp http.ResponseWriter, req *http.Request) {
-			_, done := logging.ContextLoggerRequest(req.Context(), "request to provider mirror %q", serviceName)
-			defer done()
-			resp.WriteHeader(500)
-		})
+		mux.HandleFunc(providerMirrorHandler(mirrorSvc))
 	}
 
 	httpServer := &http.Server{
@@ -58,6 +54,37 @@ func Run(ctx context.Context, config *config.Config) error {
 	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	return httpServer.Shutdown(shutdownCtx)
+}
+
+func providerMirrorHandler(cfg *config.ProviderMirror) (string, func(resp http.ResponseWriter, req *http.Request)) {
+	serviceName := cfg.Name
+	prefix := "/" + serviceName + "/"
+
+	advertiseHandler := func(resp http.ResponseWriter, req *http.Request) {
+		// TODO: A more elaborate page
+		content := "<!DOCTYPE html><html><title>Provider Mirror</title><body>This is a Terraform provider mirror.</body></html>"
+		resp.Header().Set("Content-Type", "text/html; charset=utf-8")
+		resp.Header().Set("Content-Length", strconv.FormatInt(int64(len(content)), 10))
+		resp.WriteHeader(200)
+		resp.Write([]byte(content))
+	}
+
+	return prefix, func(resp http.ResponseWriter, req *http.Request) {
+		_, done := logging.ContextLoggerRequest(req.Context(), "request to provider mirror: %s", req.URL)
+		defer done()
+
+		pathParts := strings.Split(req.URL.EscapedPath(), "/")
+		if len(pathParts) == 3 && pathParts[2] == "" {
+			// This is a request to our root, which isn't used as part of the
+			// mirror protocol and so we'll produce a HTML page advertising
+			// the mirror instead.
+			advertiseHandler(resp, req)
+			return
+		}
+
+		// TODO: Actually implement the mirror protocol.
+		resp.WriteHeader(404)
+	}
 }
 
 type contextKey string
